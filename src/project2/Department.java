0642ecf4f0ff3fca7572ff2d4634f737144d489e
GamersSquare <ismailhussein.gamer@gmail.com>
2023-05-17 12:36:56 +0300

    AddDepartment

---
+++ src/project2/Department.java
@@ -0,0 +1,36 @@
+
+package project2;
+
+import java.util.ArrayList;
+
+public class Department {
+    
+    private String id;
+    private String name;
+    private ArrayList<Instructor>instructors;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Instructor> getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(ArrayList<Instructor> instructors) {
+        this.instructors = instructors;
+    }
+    
+}
