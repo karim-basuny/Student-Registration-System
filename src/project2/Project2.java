fc8ae2cac8cdedc83654ee51f7cf2c4e7f50a571
youstena <130177804+youstena004@users.noreply.github.com>
2023-05-17 13:36:36 +0300

    main Instructor

--- src/project2/Project2.java
+++ src/project2/Project2.java
@@ -66,7 +66,14 @@ int r=input.nextInt();
         
         Department d = new Department();
         d.setInstructors(instructor);
-//end of the department  
+//end of the department
+//main Instructor
+    Instructor i1= new Instructor("ahmed",1,"dd","01204455011","cs","ds");
+      Instructor i2= new Instructor("hassan",2,"nn","01269564006","cs","ds");
+       Instructor i3= new Instructor("mohamed",3,"mm","01099345764","cs","ds");
+        Instructor i4= new Instructor("essra",4,"pp","01128945096","cs","ds");
+         Instructor i5= new Instructor("sara",5,"oo","01023789456","cs","ds");
+         //end of Instructor
 
     }
     
