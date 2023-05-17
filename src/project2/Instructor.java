1cd825236dd665c5bfb184b4742b0578a1827476
youstena <130177804+youstena004@users.noreply.github.com>
2023-05-17 12:20:57 +0300

    Add Instructor 2

--- src/project2/Instructor.java
+++ src/project2/Instructor.java
@@ -9,6 +9,59 @@ package project2;
  *
  * @author LENOVO
  */
-public class Instructor {
-    
+import java.util.Scanner;
+public class Instructor extends Person {
+    public Instructor(String name, int ID,String address,String telephone, String department , String department2) {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone_number() {
+        return telephone_number;
+    }
+
+    public void setTelephone_number(String telephone_number) {
+        this.telephone_number = telephone_number;
+    }
+
+    public void setDepartment() {
+         Scanner input =new Scanner(System.in);
+         System.out.println("How many departments you work in?");
+         int n =input.nextInt();
+         String [] d1= new String[n];
+         System.out.println("enter the departments you work in");
+         for(int i=0; i<n; i++){
+            String department= input.next(); 
+         }
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{"+"name is " + this.getName()+"\n"+ "telephone number is "+this.getTelephone_number()+"\n"+ "ID is "+this.getID()+ "\n"+"address is"+this.getAddress()+")";
+    }
+
+ 
+            
 }
